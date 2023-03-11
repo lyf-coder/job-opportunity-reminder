1ceeeea909e170342bc7af3d3ecac5a56e8a92c5
李永飞 <liyongfei@walktotop.com>
2023-03-11 11:47:45 +0800

    feat(v2ex): 实现爬取v2ex的酷工作页面的数据列表

---
+++ crawler/v2ex_test.go
@@ -0,0 +1,87 @@
+package crawler
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func Test_v2exCrawler_Crawl(t *testing.T) {
+	type fields struct {
+		PagesNum int
+		ProxyUrl string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   int
+	}{
+		// 本地执行测试-如果不能访问外网，需要设置代理，不然网络可能不通
+		{
+			name: "测试获取2个页面的招聘信息",
+			fields: fields{
+				PagesNum: 2,
+				// 为了让本地可以执行测试-github上不需要代理。为了让代码方便使用，这里使用环境变量，本地临时在环境变量里设置一下代理地址以便测试用例通过
+				ProxyUrl: os.Getenv("proxyUrl"),
+			},
+			want: 40,
+		},
+		{
+			name: "测试获取3个页面的招聘信息",
+			fields: fields{
+				PagesNum: 3,
+				ProxyUrl: os.Getenv("proxyUrl"),
+			},
+			want: 60,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			crawler := &v2exCrawler{
+				PagesNum: tt.fields.PagesNum,
+				ProxyUrl: tt.fields.ProxyUrl,
+			}
+			if got := crawler.Crawl(); !reflect.DeepEqual(len(got), tt.want) {
+				t.Errorf("Crawl() = %v, want %v", len(got), tt.want)
+			}
+		})
+	}
+}
+
+func Test_crawlPage(t *testing.T) {
+	type args struct {
+		pageNum  int
+		proxyUrl string
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		// 本地执行测试-如果不能访问外网，需要设置代理，不然网络可能不通
+		{
+			name: "测试爬取第1页",
+			args: args{
+				pageNum: 1,
+				// 为了让本地可以执行测试-github上不需要代理。为了让代码方便使用，这里使用环境变量，本地临时在环境变量里设置一下代理地址以便测试用例通过
+				proxyUrl: os.Getenv("proxyUrl"),
+			},
+			want:    20, // 期望结果数量
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := crawlPage(tt.args.pageNum, tt.args.proxyUrl)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("crawlPage() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(len(got), tt.want) {
+				t.Errorf("crawlPage() len(got) = %v, want %v", len(got), tt.want)
+			}
+		})
+	}
+}
