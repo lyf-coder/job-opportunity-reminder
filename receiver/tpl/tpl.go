15da2737527f4d4eb58051e5a485b331d255d5ef
李永飞 <liyongfei@walktotop.com>
2023-03-16 11:15:47 +0800

    feat(模版): 拆分方法实现
    暴露一些方法

--- receiver/tpl/tpl.go
+++ receiver/tpl/tpl.go
@@ -9,21 +9,32 @@ import (
 // 缓存模版
 var tplMap = make(map[string]*template.Template)
 
+func Load(name string, filePath string) (*template.Template, error) {
+	// 加载模版
+	replyTemplate, err := template.New(name).ParseFiles(filePath)
+	if err != nil {
+		log.Println("加载模版失败！", name, filePath, err)
+		return nil, err
+	} else {
+		tplMap[name] = replyTemplate
+	}
+	return replyTemplate, nil
+}
+
+// Get 获取模版
+func Get(name string, filePath string) (*template.Template, error) {
+	if replyTemplate, ok := tplMap[name]; ok {
+		return replyTemplate, nil
+	}
+	return Load(name, filePath)
+}
+
 // GetTemplateResultStr 处理模版-name是文件名
 func GetTemplateResultStr(name string, filePath string, data any) string {
-	replyTemplate, ok := tplMap[name]
-	var err error
-	if !ok {
-		log.Println("新建模版")
-		replyTemplate, err = template.New(name).ParseFiles(filePath)
-		if err != nil {
-			log.Println(err)
-		} else {
-			tplMap[name] = replyTemplate
-		}
-
+	replyTemplate, err := Get(name, filePath)
+	if err != nil {
+		log.Println("模版获取失败！", err)
 	}
-
 	replyBuff := new(bytes.Buffer)
 	err = replyTemplate.Execute(replyBuff, data)
 	if err != nil {
@@ -34,5 +45,5 @@ func GetTemplateResultStr(name string, filePath string, data any) string {
 
 // GetTplPath 获取模版文件路径-相对于main函数位置
 func GetTplPath(tmplFileName string) string {
-	return "receiver/tpl/" + tmplFileName
+	return "tpl/" + tmplFileName
 }
