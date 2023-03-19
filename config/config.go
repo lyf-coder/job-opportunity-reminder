d2339f119fcabd22feaed7838568552661fefebd
李永飞 <liyongfei@walktotop.com>
2023-03-19 14:33:11 +0800

    refactor(config): 使用yaml格式替代json格式的配置-更易读

--- config/config.go
+++ config/config.go
@@ -18,12 +18,13 @@ func LoadConfig(filePath string) {
 	// 设置文件名
 	viper.SetConfigName("config") // name of config file (without extension)
 	// 设置配置文件的类型
-	viper.SetConfigType("json") // REQUIRED if the config file does not have the extension in the name
+	viper.SetConfigType("yaml") // REQUIRED if the config file does not have the extension in the name
 
 	// 设置配置文件路径为当前工作目录
 	viper.AddConfigPath(filePath) // optionally look for config in the working directory
 
 	log.Println("加载环境变量...")
+	log.Println(viper.AllSettings())
 	// 环境变量需要大写
 	viper.AutomaticEnv()
 
